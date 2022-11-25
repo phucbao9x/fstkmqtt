be6ceff21236343ddc1d12dc3e04b03b9f42dbfe
phucbao9x <nguyentranphucbao.working@gmail.com>
2022-11-25 17:38:43 +0700

    exceptions: finished

---
+++ fstkmqtt/exceptions/__init__.py
@@ -0,0 +1,8 @@
+class MissingDataException(Exception): pass
+
+class ConnectException:
+    class DontHaveAnyConnectionException(Exception): pass
+
+class NotEnoughPrivilege(Exception): pass
+
+class NullConnectionError(Exception): pass
